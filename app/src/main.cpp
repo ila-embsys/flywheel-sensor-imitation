506217051856f5c566bb3f4a1ea504303638ded8
Ilya Stolyarov <i.stolyarov@thirdpin.ru>
2021-06-23 11:36:43 +0300

    Fix time between calls

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -41,7 +41,15 @@ extern void update_rpm(const int current_rpm, float* next_tooth_time);
 extern void time_to_tooth();
 }
 
+struct device_info {
+    struct k_work work;
+    struct k_timer timer;
+    char name[16];
+} sensor_device;
+
+
 const struct device* dev_gpio = device_get_binding(SENSOR_NODE);
+const struct device* dev_adc;
 static constexpr uint8_t CHANNEL_COUNT = 4;
 
 const struct adc_sequence sequence = {
@@ -52,44 +60,68 @@ const struct adc_sequence sequence = {
 };
 
 static const struct adc_channel_cfg m_1st_channel_cfg = {
-	.gain             = ADC_GAIN,
-	.reference        = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id       = ADC_1ST_CHANNEL_ID,
+  .gain             = ADC_GAIN,
+  .reference        = ADC_REFERENCE,
+  .acquisition_time = ADC_ACQUISITION_TIME,
+  .channel_id       = ADC_1ST_CHANNEL_ID,
 #if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
-	.input_positive   = ADC_1ST_CHANNEL_INPUT,
+  .input_positive   = ADC_1ST_CHANNEL_INPUT,
 #endif
 };
 
 static const struct device *init_adc(void)
 {
-	int ret;
+  int ret;
   const struct device *adc_dev = device_get_binding(ADC_DEVICE_NAME);
 
-	ret = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
+  ret = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
 
-	(void)memset(m_sample_buffer, 0, sizeof(m_sample_buffer));
+  (void)memset(m_sample_buffer, 0, sizeof(m_sample_buffer));
 
-	return adc_dev;
+  return adc_dev;
+}
+
+void sensor_work(struct k_work *item)
+{
+    // struct device_info *the_device =
+    //     CONTAINER_OF(item, struct device_info, work);
+    int ret;
+    static bool is_inited = false;
+
+    time_to_tooth();
+    ret = adc_read(dev_adc, &sequence);
+    float next_tooth_time;
+    const int rpm = m_sample_buffer[0];
+    update_rpm(rpm, &next_tooth_time);
+    unsigned next_tooth_time_usec = static_cast<unsigned>(next_tooth_time * 1000000);
+
+    // k_delayed_work_submit(&sensor_device.work, K_USEC(next_tooth_time_usec));
+    if (not is_inited)
+    {
+      k_timer_start(&sensor_device.timer, K_USEC(next_tooth_time_usec), K_USEC(next_tooth_time_usec));
+      is_inited = true;
+    }
+}
+
+extern void tooth_expiry_function(struct k_timer *timer_id)
+{
+    k_work_submit(&sensor_device.work);
 }
 
 void main()
 {
-    int ret;
-    const struct device* dev_adc = init_adc();
+    /* initialize name info for a device */
+    strcpy(sensor_device.name, "sensor_dev");
+
+    dev_adc = init_adc();
     gpio_pin_configure(dev_gpio, SENSOR_PIN, GPIO_OUTPUT_ACTIVE | SENSOR_FLAGS);
 
     LOG_INF("Start Ada execution!");
 
-    while (true) {
-        ret = adc_read(dev_adc, &sequence);
-        float next_tooth_time;
-        const int rpm = m_sample_buffer[0];
-        update_rpm(rpm, &next_tooth_time);
-        unsigned next_tooth_time_usec = static_cast<unsigned>(next_tooth_time * 1000000);
-        k_sleep(K_USEC(next_tooth_time_usec));
-        time_to_tooth();
-    }
+    k_work_init(&sensor_device.work, sensor_work);
+    k_timer_init(&sensor_device.timer, tooth_expiry_function, NULL);
+    k_work_submit(&sensor_device.work);
+    k_sleep(K_FOREVER);
 }
 
 extern "C"
